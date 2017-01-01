92be473876e2e4feef5e22c372d4c6ef5d5a151d
Chirantan Mitra <chirantan.mitra@gmail.com>
2017-01-01 18:04:31 +0530

    Happy new year!
    Update license details

--- client_test.go
+++ client_test.go
@@ -1,7 +1,7 @@
 // dashboard.go
 //
 // Author::    Chirantan Mitra
-// Copyright:: Copyright (c) 2015-2016. All rights reserved
+// Copyright:: Copyright (c) 2015-2017. All rights reserved
 // License::   MIT
 
 package gocd_test
